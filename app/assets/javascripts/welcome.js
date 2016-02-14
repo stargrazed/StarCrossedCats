d59fcc1bd5f22cc55a6ea075394b24c35ee482c2
Jonathan Wong <guyguy@shaw.ca>
2016-02-14 12:10:27 -0800

    ezpz data-fetching

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,14 +2,20 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
-function star(){
-  $.getJSON('https://star-api.herokuapp.com/api/v1/stars/Sun', function(data){
+function star(find_stars){
+  $.getJSON(find_stars, function(data){
     var items = [];
-    $.each(data, function(key, val) {
-        items.push(key + " ");
-        items.push(val);
+    $.each(data, function(index, val) {
+      if(val.label){
+        console.log(val);
+        items.push(index);
+        items.push(" "+val.label+" ");
+        items.push(" x: "+val.x);
+        items.push(" y: "+val.y);
+        items.push(" z: "+val.z);
         $(".star_container").append("<li style='list-style-type: none;'>"+items+"</li>");
         items = [];
+      }
     });
   });
 }
