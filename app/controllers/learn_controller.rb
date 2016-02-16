2fd416727a48636d893e176d088b4dfc58adeee8
Patrick Cheung <patrickcheungdev@gmail.com>
2016-02-15 18:11:59 -0800

    Add Fact and Learn Models

---
+++ app/controllers/learn_controller.rb
@@ -0,0 +1,6 @@
+class LearnController < ApplicationController
+  layout 'application'
+
+  def index
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216011017) do
+ActiveRecord::Schema.define(version: 20160216015703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "facts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "learns", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "maps", force: true do |t|
     t.datetime "created_at"
