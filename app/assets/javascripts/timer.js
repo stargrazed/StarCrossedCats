9f4fb01ba6b646cd598b67ac79118a62af18d4d2
Patrick Cheung <patrickcheungdev@gmail.com>
2016-03-11 22:08:20 -0800

    Display time properly with two digits

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,14 +1,35 @@
+
+
 function clickedSkymap(){
     var clickCnt = 0;
     var skymap = document.getElementById("test");
     skymap.addEventListener('click', function(event){
-        clickCnt += 1;
+        if(clickCnt != 1)
+            clickCnt += 1;
         if(clickCnt == 1)
             startTimer();
     }, false);
 }
 
 function startTimer(){
-    var x = document.getElementById("timer");
-    
+    var time = document.getElementById("timer");
+    var timerVar = setInterval(countTimer, 1000);
+    var timeInSec = 0;
+
+    function countTimer(){
+        ++timeInSec;
+        var hour = pad(Math.floor(timeInSec / 3600));
+        var minute = pad(Math.floor((timeInSec - hour * 3600) / 60));
+        var second = pad(timeInSec - (hour * 3600 + minute * 60));
+
+        time.innerHTML = hour + ":" + minute + ":" + second;
+    }
+}
+
+function pad(n){
+    var nString = n + "";
+    if(nString.length < 2)
+        return "0" + nString;
+    else
+        return  nString;
 }
