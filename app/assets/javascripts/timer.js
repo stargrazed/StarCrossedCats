4a67abdf46b48e47a07b1171e8de4627d5b7f0cd
Patrick Cheung <patrickcheungdev@gmail.com>
2016-03-11 17:47:23 -0800

    Add ability for time to 'change' after click on svg

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,1 +1,11 @@
+function clickedSkymap(){
+    var skymap = document.getElementById("test");
+    skymap.addEventListener('click', function(event){
+        startTimer();
+    }, false);
+}
 
+function startTimer(){
+    var x = document.getElementById("timer");
+    x.innerHTML = "Time has started counting";
+}
