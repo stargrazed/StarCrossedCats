3501df6f96744fc80a84dc3a60090f6437112ec9
Jonathan Wong <guyguy@shaw.ca>
2016-03-13 15:48:31 -0700

    forgot to move functions

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -47,9 +47,55 @@ function star(find_stars){
   });
 }
 
-function test(x,y, star_index){
-  var arr = [x,y];
-  star_index[0] = arr;
-  star_index[1] = arr;
-  return;
+function star2(find_stars){ //for copying into an array more easily
+  $.getJSON(find_stars, function(data){
+    var items = [];
+    $.each(data, function(index, val) {
+        console.log(val);
+        items.push("["+val.x);
+        items.push(val.y);
+        items.push(val.z+"]");
+        $(".star_container").append("<li style='list-style-type: none;'>"+items+"</li>");
+        items = [];
+    });
+  });
+}
+
+function constell(constellation){
+  $.getJSON(constellation, function(data){
+    var items2 = [];
+    $.each(data, function(index, val) {
+        console.log(val);
+        alert(val);
+        items2.push(" Name: "+val.id);
+        items2.push("   Galaxy: "+val.galaxy);
+        //$(".star_container").append("<li style='list-style-type: none;'>"+items2+"</li>");
+        items2 = [];
+    });
+  });
+}
+
+function constell_to_star(link){
+  $.getJSON(link, function(data){
+    var str = 'https://star-api.herokuapp.com/api/v1/stars?';
+    $.each(data, function(index, val) {
+        console.log(val);
+        var num = val.star_id;
+        var min = 'min[id]=';
+        var max = '&max[id]=';
+
+        num_min = parseInt(num)-1;
+        num_min = num_min.toString();
+        num_max = parseInt(num)+1;
+        num_max = num_max.toString();
+
+        min = min+num_min;
+        max = max+num_max;
+
+        var chain = str+min+max;
+        //$(".star_container").append("<li style='list-style-type: none;'>"+chain+"</li>");
+        star2(chain);
+        items3 = [];
+    });
+  });
 }
