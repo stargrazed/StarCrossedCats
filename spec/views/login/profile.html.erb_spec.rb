12cc939bc5fa610ca79ff347b09a36b3417ea8d3
Hedy Chan <hedyc@sfu.ca>
2016-03-14 23:57:46 -0700

    New Testcase Code for profile

--- spec/views/login/profile.html.erb_spec.rb
+++ spec/views/login/profile.html.erb_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe 'login/profile.html.erb' do
   it 'display correct user profile' do
-    assign(:login, User.create(name: 'Jane')
+    assign(:login,[
+      stub_model(User, :name => "Jane")
+    ])
 
     render
 
-    rendered.should have_selector("table#login_#{@user.name} tbody tr:nth-of-type(1) td:nth-of-type(1)", text:'Jane')
+    rendered.should contain("Jane")
   end
 
 end
