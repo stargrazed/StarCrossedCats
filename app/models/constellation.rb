d6dc83f0b8bd9d980a5d4674402790ee87f499d5
Patrick Cheung <patrickcheungdev@gmail.com>
2016-03-28 00:11:05 -0700

    Add validation to Constellation model

--- app/models/constellation.rb
+++ app/models/constellation.rb
@@ -1,6 +1,14 @@
 class Constellation < ActiveRecord::Base
   require 'csv'
 
+  validates :Constellation, :presence => true
+  validates :Abbreviation, :presence => true
+  validates :Family, :presence => true
+  validates :Origin, :presence => true
+  validates :Meaning, :presence => true
+  validates :Brightest_star, :presence => true
+  validates :Image, :presence => true
+
   def self.import(file)
     CSV.foreach(file.path, headers:true) do |row|
       Constellation.create! row.to_hash
