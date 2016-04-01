3ec98740333fee194cb0d20014a710c9398dc135
Patrick Cheung <patrickcheungdev@gmail.com>
2016-04-01 11:18:44 -0700

    Improve image hover location

--- app/assets/javascripts/window_view.js
+++ app/assets/javascripts/window_view.js
@@ -1,8 +1,16 @@
 var mouseX;
 var mouseY;
+var winX;
+var winY;
 
 $(document).mousemove( function(e) {
     mouseX = e.pageX;
     mouseY = e.pageY;
-    $(".enlarge:hover span").css({"top": mouseY, "left": mouseX});
+    winY = 20 + window.pageYOffset;
+    if(window.innerWidth - 600 < 600)
+        winX = 50;
+    else
+        winX = 750 + window.pageXOffset;
+
+    $(".enlarge:hover span").css({"top": winY, "left": winX});
 });
