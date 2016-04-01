bde5db9011892501d43627d2aed60affec7a8772
Patrick Cheung <patrickcheungdev@gmail.com>
2016-03-31 22:03:55 -0700

    Add ability to delete from constellation db when csv entry is removed

--- app/models/constellation.rb
+++ app/models/constellation.rb
@@ -2,16 +2,19 @@ class Constellation < ActiveRecord::Base
   require 'csv'
 
   def self.import(file)
+    constellations_to_keep = []
     CSV.foreach(file.path, headers:true) do |row|
-
       constellation_hash = row.to_hash
       constellation = Constellation.where(Abbreviation: constellation_hash["Abbreviation"])
 
-      if constellation.count == 1
+      if constellation.present?
         constellation.first.update_attributes(constellation_hash)
       else
-        Constellation.create! (constellation_hash)
+        Constellation.create!(constellation_hash)
       end
+      constellations_to_keep << constellation_hash["Abbreviation"]
     end
+    Rails.logger.debug constellations_to_keep.inspect
+    Constellation.where('constellations."Abbreviation" NOT IN (?)', constellations_to_keep).destroy_all
   end
 end
