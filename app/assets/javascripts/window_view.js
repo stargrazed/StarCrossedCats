5e9a235e0c3af197c34fc28ccd53d92fe34ca342
Patrick Cheung <patrickcheungdev@gmail.com>
2016-04-02 20:39:28 -0700

    Improve constellation image hover location

--- app/assets/javascripts/window_view.js
+++ app/assets/javascripts/window_view.js
@@ -1,16 +1,8 @@
-var mouseX;
-var mouseY;
 var winX;
 var winY;
 
 $(document).mousemove( function(e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-    winY = 20 + window.pageYOffset;
-    if(window.innerWidth - 600 < 600)
-        winX = 50;
-    else
-        winX = 750 + window.pageXOffset;
-
+    winX = window.innerWidth/3.5;
+    winY = window.innerHeight/4 + window.pageYOffset;
     $(".enlarge:hover span").css({"top": winY, "left": winX});
 });
